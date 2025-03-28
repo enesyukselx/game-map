22b55c4d2c187680d650eaebd18371c095a4874f
Enes Yüksel <enesyuksel9903@gmail.com>
2025-03-29 02:48:48 +0300

    add drawTerrainsOnMap function to render terrains on the map

---
+++ src/utils/drawTerrainsOnMap.ts
@@ -0,0 +1,36 @@
+import { VILLAGE_SIZE } from "../constants";
+import terrainImages from "../data/terrainImages";
+import { TTerrain } from "../types";
+
+export const drawTerrainsOnMap = ({
+    ctx,
+    terrains,
+    coords,
+    mapConfig,
+}: {
+    ctx: CanvasRenderingContext2D;
+    terrains: TTerrain[];
+    coords: { x: number; y: number };
+    mapConfig: { size: { width: number; height: number }; scale: number };
+}) => {
+    //
+    terrains.forEach((terrain) => {
+        //
+        let vx = (terrain.coords.x - coords.x) * mapConfig.scale;
+        let vy = (terrain.coords.y - coords.y) * mapConfig.scale;
+        const vSize = VILLAGE_SIZE * mapConfig.scale;
+
+        if (
+            vx < -vSize ||
+            vx > mapConfig.size.width + vSize ||
+            vy < -vSize ||
+            vy > mapConfig.size.height + vSize
+        ) {
+            return;
+        }
+
+        let terrainImage = terrainImages[terrain.type];
+
+        ctx.drawImage(terrainImage, vx, vy, vSize, vSize);
+    });
+};
