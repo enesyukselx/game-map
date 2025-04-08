4e4e8db891996634bc31c545f3fdbae0763744ed
enesyukselx <enesyuksel9903@gmail.com>
2025-04-08 14:15:27 +0300

    feat: replace PNG images with WEBP format for improved performance

--- src/data/terrainImages.ts
+++ src/data/terrainImages.ts
@@ -1,9 +1,9 @@
 const lakeImage = new Image();
-lakeImage.src = "assets/images/terrains/lake.png";
+lakeImage.src = "assets/images/terrains/lake.webp";
 const mineImage = new Image();
-mineImage.src = "assets/images/terrains/mine.png";
+mineImage.src = "assets/images/terrains/mine.webp";
 const forestImage = new Image();
-forestImage.src = "assets/images/terrains/forest.png";
+forestImage.src = "assets/images/terrains/forest.webp";
 
 const terrainImages = {
     lake: lakeImage,
--- src/data/villageImages.ts
+++ src/data/villageImages.ts
@@ -1,28 +1,28 @@
 const village1 = new Image();
-village1.src = "/assets/images/villages/village1.png";
+village1.src = "/assets/images/villages/village1.webp";
 const village2 = new Image();
-village2.src = "/assets/images/villages/village2.png";
+village2.src = "/assets/images/villages/village2.webp";
 const village3 = new Image();
-village3.src = "/assets/images/villages/village3.png";
+village3.src = "/assets/images/villages/village3.webp";
 const village4 = new Image();
-village4.src = "/assets/images/villages/village4.png";
+village4.src = "/assets/images/villages/village4.webp";
 const village5 = new Image();
-village5.src = "/assets/images/villages/village5.png";
+village5.src = "/assets/images/villages/village5.webp";
 const village6 = new Image();
-village6.src = "/assets/images/villages/village6.png";
+village6.src = "/assets/images/villages/village6.webp";
 //
 const village1Barbar = new Image();
-village1Barbar.src = "/assets/images/villages/village1_barbar.png";
+village1Barbar.src = "/assets/images/villages/village1_barbar.webp";
 const village2Barbar = new Image();
-village2Barbar.src = "/assets/images/villages/village2_barbar.png";
+village2Barbar.src = "/assets/images/villages/village2_barbar.webp";
 const village3Barbar = new Image();
-village3Barbar.src = "/assets/images/villages/village3_barbar.png";
+village3Barbar.src = "/assets/images/villages/village3_barbar.webp";
 const village4Barbar = new Image();
-village4Barbar.src = "/assets/images/villages/village4_barbar.png";
+village4Barbar.src = "/assets/images/villages/village4_barbar.webp";
 const village5Barbar = new Image();
-village5Barbar.src = "/assets/images/villages/village5_barbar.png";
+village5Barbar.src = "/assets/images/villages/village5_barbar.webp";
 const village6Barbar = new Image();
-village6Barbar.src = "/assets/images/villages/village6_barbar.png";
+village6Barbar.src = "/assets/images/villages/village6_barbar.webp";
 
 const villageImages = {
     1: [village1, village1Barbar],
